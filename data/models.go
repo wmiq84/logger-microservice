d5b0e116baa48f12f4c499382c021466955c6543
Michael Wang <michaelwang3489@gmail.com>
2025-06-18 10:02:06 -0700

    Add insert and return entry methods

---
+++ data/models.go
@@ -0,0 +1,92 @@
+package data
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var client *mongo.Client
+
+func New(mongo *mongo.Client) Models {
+	client = mongo
+
+	return Models{
+		LogEntry: LogEntry{},
+	}
+}
+
+type Models struct {
+	LogEntry LogEntry
+}
+
+// bson used for Mongo
+type LogEntry struct {
+	ID        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	Name      string    `bson:"name" json:"name"`
+	Data      string    `bson:"data" json:"data"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+}
+
+func (l *LogEntry) Insert(entry LogEntry) error {
+	// chooses db named logs and table called logs
+	collection := client.Database("logs").Collection("logs")
+
+	// could replace TODO with below to cancel request on client disconnect
+	// ctx, cancel := context.WithTimeout(parentCtx, 5*time.Second)
+	_, err := collection.InsertOne(context.TODO(), LogEntry{
+		Name:      entry.Name,
+		Data:      entry.Data,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	if err != nil {
+		log.Println("Error inserting into logs")
+		return err
+	}
+
+	return nil
+}
+
+// returns slice of pointers to LogEntry
+// find all sorted by created_at descending
+// retuns corresponding list of addresses of items AKA pointers to LogEntry
+func (l *LogEntry) All() ([]*LogEntry, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+
+	// if takes longer than 15s
+	defer cancel()
+
+	collection := client.Database("logs").Collection("logs")
+
+	opts := options.Find()
+	opts.SetSort(bson.D{{"created_at", -1}})
+
+	cursor, err := collection.Find(context.TODO(), bson.D{}, opts)
+	if err != nil {
+		log.Println("Finding all docs error:", err)
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	// slices of pointers to LogEntry
+	var logs []*LogEntry
+
+	for cursor.Next(ctx) {
+		var item LogEntry
+
+		err := cursor.Decode(&item)
+		if err != nil {
+			log.Print("Error decoding log into slice:", err)
+			return nil, err
+		} else {
+			logs = append(logs, &item)
+		}
+	}
+}
